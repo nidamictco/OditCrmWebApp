97005fb0154c153545ebb13c29da1031934e5506
oxdoshib <oxdo.shibina@gmail.com>
2026-04-29 14:53:12 +0530

    shibina - pulling files after project name change

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:login_2_it_solution/feature/rightside_menu/call_settings.dart/screen/call_settings.dart';
 import 'package:login_2_it_solution/feature/auth/screen/login.dart';
@@ -6,7 +7,12 @@ import 'package:login_2_it_solution/feature/rightside_menu/lead_category/lead_ca
 import 'package:login_2_it_solution/feature/sidebar/main_screen.dart';
 import 'package:sizer/sizer.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
